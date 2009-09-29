3f46964c1832c9e9f7efc8f8491adc2613d78600
William Stein <wstein@gmail.com>
2009-09-29 03:51:03 -0700

    spkg-dist work

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def all_files(dir, lstrip):
     
 
 code = setup(name = 'sagenb',
-      version     = '0.1.12',   # the spkg-dist script assumes single quotes here
+      version     = '0.1.13',   # the spkg-dist script assumes single quotes here
       description = 'The Sage Notebook',
       license     = 'GNU Public License (GPL) v2+',
       author      = 'William Stein et al.',
