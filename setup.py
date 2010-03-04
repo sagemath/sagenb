8927f20986511ed77e972dddae900989cd347d1f
Mitesh Patel <qed777@gmail.com>
2010-03-04 14:27:48 -0800

    #8435: SageNB 0.7.5.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def all_files(dir, lstrip):
     
 
 code = setup(name = 'sagenb',
-      version     = '0.7.5.1',  # the spkg-dist script assumes single quotes here
+      version     = '0.7.5.2',  # the spkg-dist script assumes single quotes here
       description = 'The Sage Notebook',
       license     = 'GNU Public License (GPL) v2+',
       author      = 'William Stein et al.',
