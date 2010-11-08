8a578e84e006cd814b7b93a022b6e7c536827427
Minh Van Nguyen <nguyenminh2@gmail.com>
2010-11-08 08:56:04 -0800

    #10118: upgrade Sphinx to upstream version 1.0.4 Update doctests to conform to Sphinx 1.0.4.

--- sagenb/misc/sphinxify.py
+++ sagenb/misc/sphinxify.py
@@ -67,11 +67,11 @@ def sphinxify(docstring, format='html'):
 
         sage: from sagenb.misc.sphinxify import sphinxify
         sage: sphinxify('A test')
-        '<div class="docstring">\n    \n  <p>A test</p>\n\n\n</div>'
+        '\n<div class="docstring">\n    \n  <p>A test</p>\n\n\n</div>'
         sage: sphinxify('**Testing**\n`monospace`')
-        '<div class="docstring"...<strong>Testing</strong>\n<span class="math"...</p>\n\n\n</div>'
+        '\n<div class="docstring"...<strong>Testing</strong>\n<span class="math"...</p>\n\n\n</div>'
         sage: sphinxify('`x=y`')
-        '<div class="docstring">\n    \n  <p><span class="math">x=y</span></p>\n\n\n</div>'
+        '\n<div class="docstring">\n    \n  <p><span class="math">x=y</span></p>\n\n\n</div>'
         sage: sphinxify('`x=y`', format='text')
         'x=y\n'
         sage: sphinxify(':math:`x=y`', format='text')
