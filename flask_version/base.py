57a0859e035fb2ec822fcd886b9f3d684f8c385f
Mike Hansen <mhansen@gmail.com>
2011-01-07 19:46:28 -0800

    Make the notebook object an attribute of the app.

--- flask_version/base.py
+++ flask_version/base.py
@@ -50,11 +50,11 @@ def index():
     if 'username' in session:
         response =  redirect(url_for('home', username=session['username']))
         if 'remember' in request.args:
-            response.set_cookie('nb_session_%s'%notebook.port,
+            response.set_cookie('nb_session_%s'%app.notebook.port,
                                 expires=(time.time() + 60 * 60 * 24 * 14))
         else:
-            response.set_cookie('nb_session_%s'%notebook.port)
-        response.set_cookie('cookie_test_%s'%notebook.port, expires=1)
+            response.set_cookie('nb_session_%s'%app.notebook.port)
+        response.set_cookie('cookie_test_%s'%app.notebook.port, expires=1)
         return response
 
     return login()
@@ -62,14 +62,14 @@ def index():
 @app.route('/home/<username>/')
 @login_required
 def home(username):
-    if username != session['username']:
+    if not app.notebook.user_is_admin(username) and username != session['username']:
         #XXX: Put this into a template
         return "User '%s' does not have permission to view the home page of '%s'."%(session['username'],
                                                                                     username)
     else:
         from sagenb.notebook.twist import render_worksheet_list
         import sagenb.notebook.twist as twist
-        twist.notebook = notebook
+        twist.notebook = app.notebook
         return render_worksheet_list(request.args, pub=False, username=session['username'])
 
 @app.route('/home/')
@@ -84,9 +84,9 @@ def bare_home():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     from sagenb.misc.misc import SAGE_VERSION
-    template_dict = {'accounts': notebook.get_accounts(),
-                     'default_user': notebook.default_user(),
-                     'recovery': notebook.conf()['email'],
+    template_dict = {'accounts': app.notebook.get_accounts(),
+                     'default_user': app.notebook.default_user(),
+                     'recovery': app.notebook.conf()['email'],
                      'sage_version':SAGE_VERSION}
 
     if request.method == 'POST':
@@ -97,16 +97,15 @@ def login():
             return "Please enable cookies or delete all Sage cookies and localhost cookies in your browser and try again."
 
         try:
-            U = notebook.user(str(username))
+            U = app.notebook.user(username)
         except KeyError:
             #log.msg("Login attempt by unknown user '%s'."%username)
             U = None
             template_dict['username_error'] = True
             
-            
         if U is None:
             pass
-        elif U.password_is(str(password)):
+        elif U.password_is(password):
             if U.is_suspended():
                 #suspended
                 return "Your account is currently suspended"
@@ -119,7 +118,7 @@ def login():
             template_dict['password_error'] = True
 
     response = app.make_response(render_template('html/login.html', **template_dict))
-    response.set_cookie('cookie_test_%s'%notebook.port, 'cookie_test')
+    response.set_cookie('cookie_test_%s'%app.notebook.port, 'cookie_test')
     return response
 
 @app.route('/logout/')
@@ -134,8 +133,8 @@ def init_updates():
     global save_interval, idle_interval, last_save_time, last_idle_time
     from sagenb.misc.misc import walltime
 
-    save_interval = notebook.conf()['save_interval']
-    idle_interval = notebook.conf()['idle_check_interval']
+    save_interval = app.notebook.conf()['save_interval']
+    idle_interval = app.notebook.conf()['idle_check_interval']
     last_save_time = walltime()
     last_idle_time = walltime()
 
@@ -151,7 +150,7 @@ if __name__ == '__main__':
     sagenb.notebook.notebook.JSMATH = True
     import sagenb.notebook.notebook as notebook
 
-    notebook = notebook.load_notebook(path_to_notebook,interface="localhost",port=port,secure=False)
+    app.notebook = notebook.load_notebook(path_to_notebook,interface="localhost",port=port,secure=False)
     SAGETEX_PATH = ""
     OPEN_MODE = False
     SID_COOKIE = str(hash(path_to_notebook))
@@ -160,5 +159,5 @@ if __name__ == '__main__':
 
     app.run(debug=True, port=port)
 
-    notebook.save()
+    app.notebook.save()
     print "Notebook saved!"
