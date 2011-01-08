42a1b1809f08f4fc00e674d31705d8075fdcc2d2
Mike Hansen <mhansen@gmail.com>
2011-01-07 20:21:05 -0800

    Split authentication off into it's own file.  Start the server from a different module.

--- flask_version/base.py
+++ flask_version/base.py
@@ -57,6 +57,7 @@ def index():
         response.set_cookie('cookie_test_%s'%app.notebook.port, expires=1)
         return response
 
+    from authentication import login
     return login()
 
 @app.route('/home/<username>/')
@@ -77,54 +78,10 @@ def home(username):
 def bare_home():
     return redirect(url_for('home', username=session['username']))
 
-##################
-# Authentication #
-##################
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    from sagenb.misc.misc import SAGE_VERSION
-    template_dict = {'accounts': app.notebook.get_accounts(),
-                     'default_user': app.notebook.default_user(),
-                     'recovery': app.notebook.conf()['email'],
-                     'sage_version':SAGE_VERSION}
-
-    if request.method == 'POST':
-        username = request.form['email']
-        password = request.form['password']
-
-        if username == 'COOKIESDISABLED':
-            return "Please enable cookies or delete all Sage cookies and localhost cookies in your browser and try again."
-
-        try:
-            U = app.notebook.user(username)
-        except KeyError:
-            #log.msg("Login attempt by unknown user '%s'."%username)
-            U = None
-            template_dict['username_error'] = True
-            
-        if U is None:
-            pass
-        elif U.password_is(password):
-            if U.is_suspended():
-                #suspended
-                return "Your account is currently suspended"
-            else:
-                #Valid user, everything is okay
-                session['username'] = username
-                session.modified = True
-                return redirect(url_for('index'))
-        else:
-            template_dict['password_error'] = True
-
-    response = app.make_response(render_template('html/login.html', **template_dict))
-    response.set_cookie('cookie_test_%s'%app.notebook.port, 'cookie_test')
-    return response
-
-@app.route('/logout/')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('index'))
+################
+# View imports #
+################
+import authentication
 
 #############
 # OLD STUFF #
@@ -138,7 +95,8 @@ def init_updates():
     last_save_time = walltime()
     last_idle_time = walltime()
 
-if __name__ == '__main__':
+#CLEAN THIS UP!
+def start():
     import sys
     path_to_notebook = sys.argv[1].rstrip('/')
     port = 5000
