e1e189fb63ae5187deaf5b1829fc465d03753e27
Jason Grout <jason.grout@drake.edu>
2011-04-16 16:47:13 -0500

    Fix references to _static directories that are in subdirectories of the manuals to point to the _static directory in the root of the manual.
    This basically is pulling forward a fix from the old twist.py.  It feels like we are working around a Sphinx bug here, so this should be looked into sometime.

--- flask_version/doc.py
+++ flask_version/doc.py
@@ -37,6 +37,27 @@ def docs_static_index():
 def doc_live_base():
     return current_app.message('nothing to see.', username=g.username)
 
+@doc.route('/doc/live/<manual>/<path:path_static>/_static/<path:filename>')
+@login_required
+def doc_static_file(manual, path_static, filename):
+    """
+    The docs reference a _static URL in the current directory, even if
+    the real _static directory only lives in the root of the manual.
+    This function strips out the subdirectory part and returns the
+    file from the _static directory in the root of the manual.
+
+    This seems like a Sphinx bug: the generated html file should not
+    reference a _static in the current directory unless there actually
+    is a _static directory there.
+
+    TODO: Determine if the reference to a _static in the current
+    directory is a bug in Sphinx, and file a report or see if it has
+    already been fixed upstream.
+    """
+    from flask.helpers import send_file
+    filename = os.path.join(DOC, manual, '_static', filename)
+    return send_file(filename)
+
 @doc.route('/doc/live/<path:filename>')
 @login_required
 def doc_live(filename):
