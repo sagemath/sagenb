403d3cedcd27955e3dcf2ee884bf279c5794b74d
Jason Grout <jason.grout@drake.edu>
2011-10-18 01:15:55 -0500

    Delete early-abort check for saving snapshots that aborts saving a snapshot if the on-disk file is the same as the in-memory file.

--- sagenb/notebook/worksheet.py
+++ sagenb/notebook/worksheet.py
@@ -1950,10 +1950,6 @@ class Worksheet(object):
         if E is None:
             E = self.edit_text()
         worksheet_html = self.worksheet_html_filename()
-        if os.path.exists(worksheet_html) and \
-            open(worksheet_html).read() == E.encode('utf-8', 'ignore'):
-            # we already wrote it out...
-            return
         open(filename, 'w').write(bz2.compress(E.encode('utf-8', 'ignore')))
         open(worksheet_html, 'w').write(self.body().encode('utf-8', 'ignore'))
         self.limit_snapshots()
