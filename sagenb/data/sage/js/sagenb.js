b24de46a9f04b7c359c5e1c7d5ef2009216d6ebe
Samuel Ainsworth <skainsworth@gmail.com>
2012-06-16 23:04:47 -0400

    report a problem working

--- sagenb/data/sage/js/sagenb.js
+++ sagenb/data/sage/js/sagenb.js
@@ -13,6 +13,7 @@ sagenb.ctrlkey = "";
 // other general variables go here
 
 sagenb.init = function() {
+	// TODO is this wrong? ie, is it possible to look at someone else's worksheet?
 	// grab the username from the url
 	sagenb.username = window.location.pathname.substring(6).split("/")[0];
 	
@@ -25,4 +26,7 @@ sagenb.init = function() {
 		sagenb.ctrlkey = "Cmd";
 	}
 	
+	$("#report_a_problem").click(function(e) {
+		window.open('http://spreadsheets.google.com/viewform?key=pCwvGVwSMxTzT6E2xNdo5fA', '', 'menubar=1,location=1,scrollbars=1,width=800,height=650,toolbar=1,resizable=1');
+	});
 };
