f32ecf86687b63221a3841e5d5a48e367310307a
Samuel Ainsworth <skainsworth@gmail.com>
2012-07-11 16:02:57 -0400

    Fix search bug in worksheet listing page

--- sagenb/data/sage/js/worksheet_list.js
+++ sagenb/data/sage/js/worksheet_list.js
@@ -351,15 +351,11 @@ sagenb.worksheetlistapp.worksheet_list = function() {
 				break;
 		}
 
-		_this.disable_actions_menu();
 		_this.load(("type=" + type + "&search=" + q), function() {
 			// TODO gettext
 			document.title = str + " - Search - Sage";
 			$(".title").text(str + " - Search");
 			$("#main_checkbox").prop("checked", false);
-
-			_this.enable_actions_menu();
-			$("#send_to_archive_button, #delete_button, #stop_button, #download_button").removeClass("disabled");
 		});
 	}
 };
