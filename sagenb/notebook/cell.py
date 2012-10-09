003436a47bc97429ea25287d2aa15fc4a2b527eb
Jason Grout <jason.grout@drake.edu>
2012-10-09 10:23:11 -0500

    Remove the remainder of the whatwg unsafe tags.
    See https://groups.google.com/forum/?fromgroups=#!msg/sage-devel/ze5PZc1Wyjk/sjc-vkYbkUIJ for a discussion.

--- sagenb/notebook/cell.py
+++ sagenb/notebook/cell.py
@@ -56,7 +56,9 @@ try:
             if el.tag=='script' and el.get('type')=='math/tex' and not el.get('src'):
                 return True
             return super(SageCleaner, self).allow_element(el)
-    html_cleaner = SageCleaner(page_structure=False, remove_tags=('head', 'title'), style=True, add_nofollow=True)
+
+    html_cleaner = SageCleaner(style=True, add_nofollow=True,
+                               remove_tags=('base', 'basefont', 'bdo', 'body', 'isindex', 'noscript'))
     def clean_html(text):
         try:
             return html_cleaner.clean_html(text)
