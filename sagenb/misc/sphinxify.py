5985758a35a08199ae76f75f08f1a19b4ab6c69d
John H. Palmieri <palmieri@math.washington.edu>
2012-11-16 10:25:52 -0800

    Reinstate 'is_html_markup'.

--- sagenb/misc/sphinxify.py
+++ sagenb/misc/sphinxify.py
@@ -30,6 +30,22 @@ except ImportError:
     SAGE_DOC = ''  # used to be None
 
 
+def is_sphinx_markup(docstring):
+    """
+    Returns whether a string that contains Sphinx-style ReST markup.
+
+    INPUT:
+
+    - ``docstring`` - string to test for markup
+
+    OUTPUT:
+
+    - boolean
+    """
+    # this could be made much more clever
+    return ("`" in docstring or "::" in docstring)
+
+
 def sphinxify(docstring, format='html'):
     r"""
     Runs Sphinx on a ``docstring``, and outputs the processed
@@ -55,7 +71,7 @@ def sphinxify(docstring, format='html'):
         sage: sphinxify('**Testing**\n`monospace`')
         '\n<div class="docstring"...<strong>Testing</strong>\n<span class="math"...</p>\n\n\n</div>'
         sage: sphinxify('`x=y`')
-        '\n<div class="docstring">\n    \n  <p><span class="math">\\(x=y\\)</span></p>\n\n\n</div>'
+        '\n<div class="docstring">\n    \n  <p><span class="math">x=y</span></p>\n\n\n</div>'
         sage: sphinxify('`x=y`', format='text')
         'x=y\n'
         sage: sphinxify(':math:`x=y`', format='text')
