cd4be1170573d8aeb9fafb7412b4679c3db42358
Punarbasu Purkayastha <ppurka@gmail.com>
2014-02-03 16:22:45 +0800

    Include a doctest to make sure #195 does not recur

--- sagenb/misc/misc.py
+++ sagenb/misc/misc.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
 """
 Miscellaneous Notebook Functions
+
+TESTS:
+
+Check that github issue #195 is fixed::
+
+    sage: from sagenb.misc.misc import mathjax_macros
+    sage: print mathjax_macros
+    ['ZZ: "\\\\Bold{Z}"',
+    'NN: "\\\\Bold{N}"',
+    'RR: "\\\\Bold{R}"',
+    'CC: "\\\\Bold{C}"',
+    'QQ: "\\\\Bold{Q}"',
+    'QQbar: "\\\\overline{\\\\QQ}"',
+    'GF: ["\\\\Bold{F}_{#1}",1]',
+    'Zp: ["\\\\ZZ_{#1}",1]',
+    'Qp: ["\\\\QQ_{#1}",1]',
+    'Zmod: ["\\\\ZZ/#1\\\\ZZ",1]',
+    'CDF: "\\\\Bold{C}"',
+    'CIF: "\\\\Bold{C}"',
+    'CLF: "\\\\Bold{C}"',
+    'RDF: "\\\\Bold{R}"',
+    'RIF: "\\\\Bold{I} \\\\Bold{R}"',
+    'RLF: "\\\\Bold{R}"',
+    'CFF: "\\\\Bold{CFF}"',
+    'Bold: ["\\\\mathbf{#1}",1]']
+
 """
 
 #############################################################################
