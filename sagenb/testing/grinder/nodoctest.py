2c5af3f67618a8b279dba08e30295e9463337fd4
kcrisman <kcrisman@gmail.com>
2014-12-08 11:40:24 -0500

    Create nodoctest.py
    This should fix the doctest errors in sagenb, as well as give a tiny bit of info to anyone seeking to use Grinder with sagenb.

---
+++ sagenb/testing/grinder/nodoctest.py
@@ -0,0 +1,10 @@
+"""
+This directory contains files related to using Grinder
+(http://grinder.sourceforge.net/) to do testing of the
+Sage notebook server. The imports necessary to even load
+these files do not work without a Grinder instance running,
+and there is also some hard-coding that you would need to
+fix to use it, so we do not run tests. However, the Grinder
+objects could be useful to someone seeking an alternative
+to Selenium testing.
+"""
