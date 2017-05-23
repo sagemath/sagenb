93e52cd5448b37a06f1f4e03b560cfaed183cda6
Dima Pasechnik <dimpase@gmail.com>
2017-05-24 00:48:00 +0100

    update to 1.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ install_requires = [
 
 
 setup(name='sagenb',
-          version     = '0.13',
+          version     = '1.0',
           description = 'The Sage Notebook',
           license     = 'GNU General Public License (GPL) v3+',
           author      = 'William Stein et al.',
